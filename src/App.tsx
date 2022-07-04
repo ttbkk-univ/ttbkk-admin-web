8399bd2cac280bb3c453cadbbedd9fed91a52ea6
gukho <gukho@liveconnect.co.kr>
2022-07-04 18:58:59 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Home from 'pages/Home'
 import Login from 'pages/Login'
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
