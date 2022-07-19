c126987fae38b50a55eb164f23136192e606c5e9
gembitious <hassamnet@gmail.com>
2022-07-19 17:22:41 +0900

    @react-oauth/google 이용한 로그인/로그아웃 기능 구현 (검증 단계 미포함)

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { CredentialResponse, GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
-const GoogleLoginButton = () => {
-  const GOOGLE_CLIENT_ID =
-    '836663858140-nf0d1j8np1m6fdomtt9g8teb5a2jhvni.apps.googleusercontent.com';
+type GoogleLoginButtonProps = {
+  onLoginSuccess?: () => void;
+  onLoginFailure?: () => void;
+};
+
+const GOOGLE_CLIENT_ID = '836663858140-nf0d1j8np1m6fdomtt9g8teb5a2jhvni.apps.googleusercontent.com';
+
+const GoogleLoginButton = (props: GoogleLoginButtonProps) => {
+  const { onLoginSuccess, onLoginFailure } = props;
+  const onSuccess = (response: CredentialResponse) => {
+    // console.log(response);
+    // 서버로 credential 보내는 코드 위치
+
+    onLoginSuccess?.();
+  };
+  const onFailure = () => {
+    console.log('Login Failed');
+    onLoginFailure?.();
+  };
+
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <GoogleLogin
-        onSuccess={credentialResponse => {
-          console.log(credentialResponse);
-        }}
-        onError={() => {
-          console.log('Login Failed');
-        }}
-      />
+      <GoogleLogin onSuccess={onSuccess} onError={onFailure} />
     </GoogleOAuthProvider>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
 import '../App.css';
 import GoogleLoginButton from 'components/GoogleLoginButton';
+import { googleLogout } from '@react-oauth/google';
 
 function Home() {
-  const [user, setUser] = useState({ email: '', id: '' });
+  const [isLogged, setIsLogged] = useState(false);
+
+  const onClickGoogleLogout = () => {
+    if (isLogged) {
+      googleLogout();
+      setIsLogged(false);
+      alert('Logout success!');
+    } else {
+      alert('Not logged now');
+    }
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <GoogleLoginButton />
-      </header>
+      <header className="App-header">{isLogged ? 'Logged' : 'Not Logged'}</header>
+      <div className="App-body">
+        <GoogleLoginButton
+          onLoginSuccess={() => {
+            setIsLogged(true);
+            alert('Login success!');
+          }}
+        />
+        <button className="button" onClick={onClickGoogleLogout}>
+          Google Logout
+        </button>
+      </div>
     </div>
   );
 }
